484d77e8ae1535526be156311a95c01fc44fc3e7
Joe <joedsonfera@hotmail.com>
2023-08-17 08:23:13 -0300

    movement update

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ canvas.width = innerWidth
 canvas.height = innerWidth
 
 class Boundary {
-    //     static width = 40
-    //     static height = 40
+    static width = 40
+    static height = 40
     constructor({ position }) {
         this.position = position
         this.width = 40
@@ -19,6 +19,55 @@ class Boundary {
     }
 }
 
+class Player {
+    constructor({ position, velocity }) {
+        this.position = position
+        this.velocity = velocity
+        this.radius = 15
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'yellow'
+        c.fill()
+        c.closePath()
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
+const boundaries = []
+const player = new Player({
+    position: {
+        x: Boundary.width + Boundary.width / 2,
+        y: Boundary.height + Boundary.height / 2
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
 const map = [
     ['-', '-', '-', '-', '-', '-'],
     ['-', ' ', ' ', ' ', ' ', '-'],
@@ -27,8 +76,6 @@ const map = [
     ['-', '-', '-', '-', '-', '-']
 ]
 
-const boundaries = []
-
 map.forEach((row, i) => {
     row.forEach((symbol, j) => {
         switch (symbol) {
@@ -36,15 +83,69 @@ map.forEach((row, i) => {
                 boundaries.push(
                     new Boundary({
                         position: {
-                            x: 40 * j,
-                            y: 40 * i
+                            x: Boundary.width * j,
+                            y: Boundary.height * i
                         }
-                    }))
+                    })
+                )
                 break
         }
     })
 })
 
-boundaries.forEach((boundary) => {
-    boundary.draw()
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    boundaries.forEach((boundary) => {
+        boundary.draw()
+    })
+    player.update()
+    player.velocity.x = 0
+    player.velocity.y = 0
+
+    if (keys.w.pressed) {
+        player.velocity.y = -5
+    } else if (keys.a.pressed) {
+        player.velocity.x = -5
+    } else if (keys.s.pressed) {
+        player.velocity.y = 5
+    } else if (keys.d.pressed) {
+        player.velocity.x = 5
+    }
+}
+
+animate()
+
+addEventListener('keydown', ({ key }) => {
+    switch (key) {
+        case 'w':
+            keys.w.pressed = true
+            break
+        case 'a':
+            keys.a.pressed = true
+            break
+        case 's':
+            keys.s.pressed = true
+            break
+        case 'd':
+            keys.d.pressed = true
+            break
+    }
+})
+
+addEventListener('keyup', ({ key }) => {
+    switch (key) {
+        case 'w':
+            keys.w.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 's':
+            keys.s.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
+            break
+    }
 })
