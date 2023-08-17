0ff9449e58af060a77808a79623bd64201101e23
Joe <joedsonfera@hotmail.com>
2023-08-17 07:28:50 -0300

    map update

--- index.js
+++ index.js
@@ -3,3 +3,48 @@ const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerWidth
+
+class Boundary {
+    //     static width = 40
+    //     static height = 40
+    constructor({ position }) {
+        this.position = position
+        this.width = 40
+        this.height = 40
+    }
+
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const map = [
+    ['-', '-', '-', '-', '-', '-'],
+    ['-', ' ', ' ', ' ', ' ', '-'],
+    ['-', ' ', '-', '-', ' ', '-'],
+    ['-', ' ', ' ', ' ', ' ', '-'],
+    ['-', '-', '-', '-', '-', '-']
+]
+
+const boundaries = []
+
+map.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        switch (symbol) {
+            case '-':
+                boundaries.push(
+                    new Boundary({
+                        position: {
+                            x: 40 * j,
+                            y: 40 * i
+                        }
+                    }))
+                break
+        }
+    })
+})
+
+boundaries.forEach((boundary) => {
+    boundary.draw()
+})
